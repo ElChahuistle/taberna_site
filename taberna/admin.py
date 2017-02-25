4b33d1e3ed00652b9094611ebfc3f3354ced759c
Chahuistle <gerardo.thomas@gmail.com>
2017-02-25 14:25:49 -0600

    Model Completion and Admin Setup

--- taberna/admin.py
+++ taberna/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tipo, Pais_de_Origen, Ceveceria, Cerveza, Caracteristica
 
-# Register your models here.
+
+admin.site.register(Tipo)
+admin.site.register(Pais_de_Origen)
+admin.site.register(Ceveceria)
+admin.site.register(Cerveza)
+admin.site.register(Caracteristica)
--- taberna/models.py
+++ taberna/models.py
@@ -1,28 +1,58 @@
 from __future__ import unicode_literals
-# -*- coding: utf-8 -*-
-import os, sys
 from django.db import models
 
 
 class Tipo(models.Model):
     tipo = models.CharField(max_length=15)
 
-
-class Ceveceria(models.Model):
-    ceveceria = models.CharField(
-        'Cerveceria'
-        , max_length=15
-    )
+    def __str__(self):
+        return self.tipo
 
 
 class Pais_de_Origen(models.Model):
     name = models.CharField('Pais', max_length=15)
 
+    def __str__(self):
+        return self.name
+
+class Ceveceria(models.Model):
+    ceveceria = models.CharField(
+        'Cerveceria'
+        , max_length=25
+    )
+    pais_de_origen = models.ForeignKey(Pais_de_Origen)
+
+    def __str__(self):
+        return self.ceveceria
+
+
+class Caracteristica(models.Model):
+    caracteristica = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.caracteristica
+
 
 class Cerveza(models.Model):
     name = models.CharField(max_length=25)
     tipo = models.ForeignKey(Tipo)
-    cerveza = models.ForeignKey(Ceveceria)
+    cerveceria = models.ForeignKey(Ceveceria)
+    caracteristicas = models.ManyToManyField(Caracteristica)
     grados = models.CharField(max_length=5)
     precio = models.FloatField(default=0)
-    existencia = models.IntegerField(default=0)
+    on_hand = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+    def existencia(self):
+        return self.on_hand > 0
+
+
+class Mostrar(models.Model):
+    cerveza = models.ForeignKey(Cerveza)
+    mostrar = models.BooleanField(default=False)
+    orden = models.IntegerField(default=1)
+
+    def mostrar_cerveza(self):
+        return self.cerveza.existencia()
